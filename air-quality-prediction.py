9cb5bbd76698f905b0e144b9276f9ad222308310
AnkitShahi69 <117616841+AnkitShahi69@users.noreply.github.com>
2023-06-01 20:54:44 +0545

    Add files via upload

---
+++ air-quality-prediction.py
@@ -0,0 +1,493 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+## Importing necessary libraries
+
+
+# In[3]:
+
+
+import warnings
+
+import numpy as np
+import pandas as pd
+
+
+warnings.filterwarnings("ignore")
+
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+
+# In[4]:
+
+
+df=pd.read_csv('data.csv',encoding='unicode_escape')
+# Reading the dataset
+
+
+
+
+
+
+df.drop(['agency'],axis=1,inplace=True)
+df.drop(['stn_code'],axis=1,inplace=True)
+df.drop(['date'],axis=1,inplace=True)
+df.drop(['sampling_date'],axis=1,inplace=True)
+df.drop(['location_monitoring_station'],axis=1,inplace=True)
+# Dropping unnecessary columns
+
+
+
+df['location']=df['location'].fillna(df['location'].mode()[0])
+df['type']=df['type'].fillna(df['type'].mode()[0])
+# Null value Imputation for categorical data
+
+
+
+df.fillna(0, inplace=True)
+# null values are replaced with zeros for the numerical data
+
+
+
+# # CALCULATE AIR QUALITY INDEX FOR SO2 BASED ON FORMULA
+# The air quality index is a piecewise linear function of the pollutant concentration. At the boundary between AQI categories, there is a discontinuous jump of one AQI unit. To convert from concentration to AQI this equation is used
+
+# ### Function to calculate so2 individual pollutant index(si)
+
+# In[ ]:
+
+
+def cal_SOi(so2):
+    si=0
+    if (so2<=40):
+     si= so2*(50/40)
+    elif (so2>40 and so2<=80):
+     si= 50+(so2-40)*(50/40)
+    elif (so2>80 and so2<=380):
+     si= 100+(so2-80)*(100/300)
+    elif (so2>380 and so2<=800):
+     si= 200+(so2-380)*(100/420)
+    elif (so2>800 and so2<=1600):
+     si= 300+(so2-800)*(100/800)
+    elif (so2>1600):
+     si= 400+(so2-1600)*(100/800)
+    return si
+df['SOi']=df['so2'].apply(cal_SOi)
+data= df[['so2','SOi']]
+data.head()
+# calculating the individual pollutant index for so2(sulphur dioxide)
+
+
+# ### Function to calculate no2 individual pollutant index(ni)
+
+# In[ ]:
+
+
+def cal_Noi(no2):
+    ni=0
+    if(no2<=40):
+     ni= no2*50/40
+    elif(no2>40 and no2<=80):
+     ni= 50+(no2-40)*(50/40)
+    elif(no2>80 and no2<=180):
+     ni= 100+(no2-80)*(100/100)
+    elif(no2>180 and no2<=280):
+     ni= 200+(no2-180)*(100/100)
+    elif(no2>280 and no2<=400):
+     ni= 300+(no2-280)*(100/120)
+    else:
+     ni= 400+(no2-400)*(100/120)
+    return ni
+df['Noi']=df['no2'].apply(cal_Noi)
+data= df[['no2','Noi']]
+data.head()
+# calculating the individual pollutant index for no2(nitrogen dioxide)
+
+
+# ### Function to calculate rspm individual pollutant index(rpi)
+
+# In[ ]:
+
+
+def cal_RSPMI(rspm):
+    rpi=0
+    if(rpi<=30):
+     rpi=rpi*50/30
+    elif(rpi>30 and rpi<=60):
+     rpi=50+(rpi-30)*50/30
+    elif(rpi>60 and rpi<=90):
+     rpi=100+(rpi-60)*100/30
+    elif(rpi>90 and rpi<=120):
+     rpi=200+(rpi-90)*100/30
+    elif(rpi>120 and rpi<=250):
+     rpi=300+(rpi-120)*(100/130)
+    else:
+     rpi=400+(rpi-250)*(100/130)
+    return rpi
+df['Rpi']=df['rspm'].apply(cal_RSPMI)
+data= df[['rspm','Rpi']]
+data.head()
+# calculating the individual pollutant index for rspm(respirable suspended particualte matter concentration)
+
+
+# ### Function to calculate spm individual pollutant index(spi)
+
+# In[ ]:
+
+
+def cal_SPMi(spm):
+    spi=0
+    if(spm<=50):
+     spi=spm*50/50
+    elif(spm>50 and spm<=100):
+     spi=50+(spm-50)*(50/50)
+    elif(spm>100 and spm<=250):
+     spi= 100+(spm-100)*(100/150)
+    elif(spm>250 and spm<=350):
+     spi=200+(spm-250)*(100/100)
+    elif(spm>350 and spm<=430):
+     spi=300+(spm-350)*(100/80)
+    else:
+     spi=400+(spm-430)*(100/430)
+    return spi
+   
+df['SPMi']=df['spm'].apply(cal_SPMi)
+data= df[['spm','SPMi']]
+data.head()
+# calculating the individual pollutant index for spm(suspended particulate matter)
+
+
+# ### function to calculate the air quality index (AQI) of every data value
+
+# In[ ]:
+
+
+def cal_aqi(si,ni,rspmi,spmi):
+    aqi=0
+    if(si>ni and si>rspmi and si>spmi):
+     aqi=si
+    if(ni>si and ni>rspmi and ni>spmi):
+     aqi=ni
+    if(rspmi>si and rspmi>ni and rspmi>spmi):
+     aqi=rspmi
+    if(spmi>si and spmi>ni and spmi>rspmi):
+     aqi=spmi
+    return aqi
+
+df['AQI']=df.apply(lambda x:cal_aqi(x['SOi'],x['Noi'],x['Rpi'],x['SPMi']),axis=1)
+data= df[['state','SOi','Noi','Rpi','SPMi','AQI']]
+data.head()
+# Caluclating the Air Quality Index.
+
+
+# In[ ]:
+
+
+def AQI_Range(x):
+    if x<=50:
+        return "Good"
+    elif x>50 and x<=100:
+        return "Moderate"
+    elif x>100 and x<=200:
+        return "Poor"
+    elif x>200 and x<=300:
+        return "Unhealthy"
+    elif x>300 and x<=400:
+        return "Very unhealthy"
+    elif x>400:
+        return "Hazardous"
+
+df['AQI_Range'] = df['AQI'] .apply(AQI_Range)
+df.head()
+# Using threshold values to classify a particular values as good, moderate, poor, unhealthy, very unhealthy and Hazardous
+
+
+# In[ ]:
+
+
+df['AQI_Range'].value_counts()
+# These are the counts of values present in the AQI_Range column.
+
+
+# ### Splitting the dataset into Dependent and Independent columns
+
+# In[ ]:
+
+
+X=df[['SOi','Noi','Rpi','SPMi']]
+Y=df['AQI']
+X.head()
+# we only select columns like soi, noi, rpi, spmi
+
+
+# In[ ]:
+
+
+Y.head()
+# the AQI column is the target column
+
+
+# In[ ]:
+
+
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,random_state=70)
+print(X_train.shape,X_test.shape,Y_train.shape,Y_test.shape)
+# splitting the data into training and testing data
+
+
+# ### Linear Regression
+
+# In[ ]:
+
+
+model=LinearRegression()
+model.fit(X_train,Y_train)
+
+
+# In[ ]:
+
+
+#predicting train
+train_pred=model.predict(X_train)
+#predicting on test
+test_pred=model.predict(X_test)
+
+
+# In[ ]:
+
+
+RMSE_train=(np.sqrt(metrics.mean_squared_error(Y_train,train_pred)))
+RMSE_test=(np.sqrt(metrics.mean_squared_error(Y_test,test_pred)))
+print("RMSE TrainingData = ",str(RMSE_train))
+print("RMSE TestData = ",str(RMSE_test))
+print('-'*50)
+print('RSquared value on train:',model.score(X_train, Y_train))
+print('RSquared value on test:',model.score(X_test, Y_test))
+
+
+# ### Decision Tree Regressor
+
+# In[ ]:
+
+
+DT=DecisionTreeRegressor()
+DT.fit(X_train,Y_train)
+
+
+# In[ ]:
+
+
+#predicting train
+train_preds=DT.predict(X_train)
+#predicting on test
+test_preds=DT.predict(X_test)
+
+
+# In[ ]:
+
+
+RMSE_train=(np.sqrt(metrics.mean_squared_error(Y_train,train_preds)))
+RMSE_test=(np.sqrt(metrics.mean_squared_error(Y_test,test_preds)))
+print("RMSE TrainingData = ",str(RMSE_train))
+print("RMSE TestData = ",str(RMSE_test))
+print('-'*50)
+print('RSquared value on train:',DT.score(X_train, Y_train))
+print('RSquared value on test:',DT.score(X_test, Y_test))
+
+
+# ### Random Forest Regressor
+
+# In[ ]:
+
+
+RF=RandomForestRegressor().fit(X_train,Y_train)
+
+
+# In[ ]:
+
+
+#predicting train
+train_preds1=RF.predict(X_train)
+#predicting on test
+test_preds1=RF.predict(X_test)
+
+
+# In[ ]:
+
+
+RMSE_train=(np.sqrt(metrics.mean_squared_error(Y_train,train_preds1)))
+RMSE_test=(np.sqrt(metrics.mean_squared_error(Y_test,test_preds1)))
+print("RMSE TrainingData = ",str(RMSE_train))
+print("RMSE TestData = ",str(RMSE_test))
+print('-'*50)
+print('RSquared value on train:',RF.score(X_train, Y_train))
+print('RSquared value on test:',RF.score(X_test, Y_test))
+
+
+# # Classification Algorithms
+
+# In[ ]:
+
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+
+
+# In[ ]:
+
+
+X2 = df[['SOi','Noi','Rpi','SPMi']]
+Y2 = df['AQI_Range']
+# Splitting the data into independent and dependent columns for classification 
+
+
+# In[ ]:
+
+
+X_train2, X_test2, Y_train2, Y_test2 = train_test_split(X2, Y2, test_size=0.33, random_state=70)
+# Splitting the data into training and testing data 
+
+
+# ### Logistic Regression
+
+# In[ ]:
+
+
+#fit the model on train data 
+log_reg = LogisticRegression().fit(X_train2, Y_train2)
+
+#predict on train 
+train_preds2 = log_reg.predict(X_train2)
+#accuracy on train
+print("Model accuracy on train is: ", accuracy_score(Y_train2, train_preds2))
+
+#predict on test
+test_preds2 = log_reg.predict(X_test2)
+#accuracy on test
+print("Model accuracy on test is: ", accuracy_score(Y_test2, test_preds2))
+print('-'*50)
+
+# Kappa Score.
+print('KappaScore is: ', metrics.cohen_kappa_score(Y_test2,test_preds2))
+
+
+# In[ ]:
+
+
+log_reg.predict([[727,327.55,78.2,100]]) 
+
+
+# In[ ]:
+
+
+log_reg.predict([[2.7,45,35.16,23]]) 
+
+
+# In[ ]:
+
+
+log_reg.predict([[10,2.8,82,20]]) 
+
+
+# In[ ]:
+
+
+log_reg.predict([[2,45.8,37,32]])
+
+
+# ### Decision Tree Classifier
+
+# In[ ]:
+
+
+#fit the model on train data 
+DT2 = DecisionTreeClassifier().fit(X_train2,Y_train2)
+
+#predict on train 
+train_preds3 = DT2.predict(X_train2)
+#accuracy on train
+print("Model accuracy on train is: ", accuracy_score(Y_train2, train_preds3))
+
+#predict on test
+test_preds3 = DT2.predict(X_test2)
+#accuracy on test
+print("Model accuracy on test is: ", accuracy_score(Y_test2, test_preds3))
+print('-'*50)
+
+# Kappa Score
+print('KappaScore is: ', metrics.cohen_kappa_score(Y_test2,test_preds3))
+
+
+# ### Random Forest Classifier
+
+# In[ ]:
+
+
+#fit the model on train data 
+RF=RandomForestClassifier().fit(X_train2,Y_train2)
+#predict on train 
+train_preds4 = RF.predict(X_train2)
+#accuracy on train
+print("Model accuracy on train is: ", accuracy_score(Y_train2, train_preds4))
+
+#predict on test
+test_preds4 = RF.predict(X_test2)
+#accuracy on test
+print("Model accuracy on test is: ", accuracy_score(Y_test2, test_preds4))
+print('-'*50)
+
+# Kappa Score
+
+
+# ### K-Nearest Neighbours
+
+# In[ ]:
+
+
+#fit the model on train data 
+KNN = KNeighborsClassifier().fit(X_train2,Y_train2)
+#predict on train 
+train_preds5 = KNN.predict(X_train2)
+#accuracy on train
+print("Model accuracy on train is: ", accuracy_score(Y_train2, train_preds5))
+
+#predict on test
+test_preds5 = KNN.predict(X_test2)
+#accuracy on test
+print("Model accuracy on test is: ", accuracy_score(Y_test2, test_preds5))
+print('-'*50)
+
+# Kappa Score
+print('KappaScore is: ', metrics.cohen_kappa_score(Y_test2,test_preds5))
+
+
+# In[ ]:
+
+
+KNN.predict([[7.4,47.7,78.182,100]]) 
+# Predictions on random values
+
+
+# In[ ]:
+
+
+KNN.predict([[1,1.2,3.12,0]]) 
+# Predictions on random values
+
+
+# In[ ]:
+
+
+KNN.predict([[325.7,345,798.182,203]]) 
+# Predictions on random values
+
