b8218537adaa1bfc20889b0d0c159d9b1235421d
AnkitShahi69 <117616841+AnkitShahi69@users.noreply.github.com>
2023-06-01 21:06:19 +0545

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+# Create flask app
+app = Flask(__name__)
+model = pickle.load(open("aqi-model.pkl", "rb"))
+
+@app.route("/")
+def Home():
+    return render_template("predict.html")
+
+@app.route("/predict", methods = ["POST"])
+def predict():
+    noi = request.form.get('noi')
+    soi = request.form.get('soi')
+    rpi = request.form.get('rpi')
+    spmi = request.form.get('spmi')
+
+    input_query = np.array([[noi, soi, rpi, spmi]])
+
+    result = model.predict(input_query)[0]
+
+    return jsonify({'result': result})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
